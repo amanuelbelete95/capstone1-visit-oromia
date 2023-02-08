1d7727d9be33476829c621de3639406de8f99864
Amanuel Belete <amanuelbelete95@gmail.com>
2023-02-08 20:53:03 +0300

    Fix the linter errors for all files

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ const closeBtn = document.querySelector('.close-btn');
 const humBurger = document.querySelector('.hum-burger');
 const menuItems = document.querySelectorAll('.menu-mobile a');
 
-humBurger.addEventListener('click', () => 
-menu.classList.toggle('hidden-menu'));
+humBurger.addEventListener('click', () => menu.classList.toggle('hidden-menu'));
 
 closeBtn.addEventListener('click', () => {
   menu.classList.toggle('hidden-menu');
-})
+});
 
 function menuRemover() {
   menu.classList.toggle('hidden-menu');
@@ -18,66 +17,58 @@ menuItems.forEach((item) => {
   item.addEventListener('click', menuRemover);
 });
 
-
 // speaker-Dynamic
 
-
 const objSpeakers = [{
-  name: "Abiy Ahmed",
-  speakerImg: "images/abiy.jpg",
-  profession: "Ethiopian Prime Minister",
-  statement: "Oromia region is a region with potentaial resource with deep cultural diversity"
-}, 
+  name: 'Abiy Ahmed',
+  speakerImg: 'images/abiy.jpg',
+  profession: 'Ethiopian Prime Minister',
+  statement: 'Oromia region is a region with potentaial resource with deep cultural diversity',
+},
 
 {
-  name:"Denzel Washington",
-  speakerImg: "images/denzel.jpg",
-  profession:"An American actor and filmmaker",
-  statement:"When I visited Bale National Park of Oromia, I couldn't believe that these amazing natural beauty existed in the region. Different animals, birds and trees took my eyes."
+  name: 'Denzel Washington',
+  speakerImg: 'images/denzel.jpg',
+  profession: 'An American actor and filmmaker',
+  statement: "When I visited Bale National Park of Oromia, I couldn't believe that these amazing natural beauty existed in the region. Different animals, birds and trees took my eyes.",
 
 },
 
 {
-  name: "Derartu Tullu",
-  speakerImg: "images/derartu.jpg",
-  profession: "An Ethiopian Athelete",
-  statement: "Oromia is a land of beauty filled with natural heritage. I invite all my friends from all over the world to visit oromia.",
+  name: 'Derartu Tullu',
+  speakerImg: 'images/derartu.jpg',
+  profession: 'An Ethiopian Athelete',
+  statement: 'Oromia is a land of beauty filled with natural heritage. I invite all my friends from all over the world to visit oromia.',
 
 },
 
 {
-  name:"Uhuru Kenyata",
-  speakerImg: "images/uhuru2.jpg",
-  profession: "Former Kenyan President",
-  statement: "Oromia region is a region with potentaial resource with deep cultural diversity",
+  name: 'Uhuru Kenyata',
+  speakerImg: 'images/uhuru2.jpg',
+  profession: 'Former Kenyan President',
+  statement: 'Oromia region is a region with potentaial resource with deep cultural diversity',
 
 },
 
 {
-  name:"Paul Kagamae",
-  speakerImg:"images/kagame.jpg",
-  profession:"Rwandian President",
-  statement: "I was honoured to visit oromia on June 2016 when I went to Ethiopia for Economic Forum.",
+  name: 'Paul Kagamae',
+  speakerImg: 'images/kagame.jpg',
+  profession: 'Rwandian President',
+  statement: 'I was honoured to visit oromia on June 2016 when I went to Ethiopia for Economic Forum.',
 },
 
 {
-  name:"Barack Obama",
-  speakerImg:"images/obama.jpg",
-  profession:"44th U.S. President",
-  statement:"Oromia, one of the most beautiful place I have ever been to in my entire life. The weather and the natural beauty of the place is amazing."
-}
+  name: 'Barack Obama',
+  speakerImg: 'images/obama.jpg',
+  profession: '44th U.S. President',
+  statement: 'Oromia, one of the most beautiful place I have ever been to in my entire life. The weather and the natural beauty of the place is amazing.',
+},
 
-]
-
-
-
+];
 
 const featureSpeakers = document.querySelector('.feature-container');
 
-
-for(let i = 0; i < objSpeakers.length; i += 1)
-{
-  
+for (let i = 0; i < objSpeakers.length; i += 1) {
   featureSpeakers.innerHTML += `<div class="feature">
         <img src="${objSpeakers[i].speakerImg}" alt="" class="img-speaker" />
 
@@ -87,11 +78,5 @@ for(let i = 0; i < objSpeakers.length; i += 1)
         <p class="speaker-words">${objSpeakers[i].statement}
         </p>
        </div>
-      </div>`
-      ;
-
+      </div>`;
 }
-
-
-
-
